4ef6b4546396e6b4291c10e6a6029b97bd6ff930
Galahad L <galahadl06@outlook.com>
2024-10-07 15:45:23 +0800

    Initial Commit

---
+++ Duo Calc/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Duo Calc
+//
+//  Created by Galahad L on 2024/10/7.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
