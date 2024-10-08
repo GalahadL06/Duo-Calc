bad5289d04130e6ef6393ace66e3563cb444cf5e
Galahad L <galahadl06@outlook.com>
2024-10-08 20:18:52 +0800

    Rewrite "Min"&"Max" code using function.Enhance interface.

--- Duo Calc/main.c
+++ Duo Calc/main.c
@@ -7,34 +7,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <unistd.h>
+float max(float a,float b);
+float min(float a,float b);
 int main(int argc, const char * argv[]) {
     float a = 1;
     float b = 1;
     char c = '0';
+    int count = 0;
+    printf("Welcome to use Duo-Calc!\n");
+    sleep(1);
     while(1){
-        printf("Input an operator:");
+        printf("Choose from '+' '-' '*' '/' '%' '!' '^'\nPress '$' to exit\nInput an operator:");
         scanf("%c",&c);
         if(c=='M'){
             printf("Input a:");
             scanf("%f",&a);
             printf("Input b:");
             scanf("%f",&b);
-            if (a>b) {
-                printf("a = %f\n",a);
-            } else {
-                printf("b = %f\n",b);
-            }
+            float i =max(a,b);
+            printf("Max = %f\n",i);
         }
         else if(c=='m'){
             printf("Input a:");
             scanf("%f",&a);
             printf("Input b:");
             scanf("%f",&b);
-            if (a<b) {
-                printf("a = %f\n",a);
-            } else {
-                printf("b = %f\n",b);
-            }
+            float i = min(a,b);
+            printf("Min = %f\n",i);
         }
         else if(c=='+'){
             printf("Input a:");
@@ -108,8 +108,30 @@ int main(int argc, const char * argv[]) {
         else{
             printf("Invalid Operator\n");
         }
+        printf("\n");
         getchar();
+        count++;
+        sleep(1);
     }
+    printf("\n********** Program Ended **********\n");
+    printf("Total Circulation:%d\n",count);
     return 0;
 }
-
+float max(float a,float b){
+    float max;
+    if (a>b) {
+        max = a;
+    } else {
+        max = b;
+    }
+    return max;
+}
+float min(float a,float b){
+    float min;
+    if (a<b) {
+        min = a;
+    } else {
+        min = b;
+    }
+    return min;
+}
